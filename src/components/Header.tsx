20659f18063e68cbd18295ac1c05047056c440aa
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2024-09-25 13:22:13 +0200

    Initial Commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import styles from 'Header.module.scss';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.header__inner}></div>
+    </header>
+  );
+};
+
+export default Header;
