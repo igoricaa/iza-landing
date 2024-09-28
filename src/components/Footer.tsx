331fbc35f590f2f84e6f67d4a073d6f272bbbc76
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2024-09-28 14:52:06 +0200

    copyright font

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,10 +3,12 @@ const Footer = () => {
     <footer className='mt-auto p-6 text-secondary lg:fixed lg:bottom-0 lg:left-0 w-full'>
       <div className='flex justify-between text-xs font-light uppercase -mt-[.5vw]'>
         <span className='ts tsf'>
-          <span className='tsl'>COPYRIGHT @ IZA</span>
+          <span className='tsl'>
+            Copyright <span className="text-[0.5rem]">©</span> IZA
+          </span>
         </span>
         <span className='ts tsf'>
-          <span className='tsl'>TEMPORARY WEBSITE</span>
+          <span className='tsl'>Temporary website</span>
         </span>
       </div>
     </footer>
