1534c149fe306de6a4f18a95d058532751917ed2
Igor Stanisavljevic <stanisavljevic.igor@proton.me>
2024-10-23 10:18:07 +0200

    Cal setup

---
+++ src/components/CalButton.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { getCalApi } from '@calcom/embed-react';
+import { useEffect } from 'react';
+
+export default function CalButton({ className }: { className?: string }) {
+  useEffect(() => {
+    (async function () {
+      const cal = await getCalApi({ namespace: 'discovery-session' });
+      cal('ui', {
+        theme: 'light',
+        styles: { branding: { brandColor: '#000000' } },
+        hideEventTypeDetails: false,
+        layout: 'month_view',
+      });
+    })();
+  }, []);
+
+  return (
+    <button
+      className={[className].join(' ')}
+      data-cal-namespace='discovery-session'
+      data-cal-link='iza.rs/discovery-session'
+      data-cal-config='{"layout":"month_view"}'
+      style={{ outline: 'none', cursor: 'pointer' }}
+    >
+      <span className='ts tsf group-hover:delay-750'>
+        <span className='tsl text-nowrap'>Grab a coffee?</span>
+      </span>
+      <span className='ts tsf group-hover:delay-750'>
+        <span className='tsl text-nowrap'>info@iza.rs</span>
+      </span>
+    </button>
+  );
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,26 +1,18 @@
+import CalButton from './CalButton';
 import styles from './Header.module.scss';
 
 const Header = () => {
   return (
     <header className='lg:fixed lg:top-0 lg:left-0 w-full'>
       <div className='grid grid-cols-5 gap-y-6 p-6 mx-auto'>
-        <div className={`${styles.header__logo__wrapper} col-span-2 sm:col-span-1`}>
+        <div
+          className={`${styles.header__logo__wrapper} col-span-2 sm:col-span-1`}
+        >
           <h1 className={`${styles.header__logo__wrapper__logo} ts tsf`}>
             <span className='tsl'>IZA</span>
           </h1>
         </div>
-        <div className='row-start-2 sm:row-start-1 sm:col-start-2 flex flex-col w-fit items-center md:mr-[8vw] border-white transition-[border] duration-1000 p-2 border-2 [&:has(.tsl.tsa)]:border-main border-solid group hover:bg-main'>
-          <span className='ts tsf group-hover:delay-750'>
-            <a href='mailto:info@iza.rs'>
-              <span className='tsl text-nowrap'>Grab a coffee?</span>
-            </a>
-          </span>
-          <span className='ts tsf group-hover:delay-750'>
-            <a href='mailto:info@iza.rs'>
-              <span className='tsl text-nowrap'>info@iza.rs</span>
-            </a>
-          </span>
-        </div>
+        <CalButton className='row-start-2 sm:row-start-1 sm:col-start-2 flex flex-col w-fit items-center md:mr-[8vw] border-white transition-[border] duration-1000 p-2 border-2 [&:has(.tsl.tsa)]:border-main border-solid group hover:bg-main' />
         <div className='col-span-2 col-start-5 flex flex-col w-fit justify-end justify-self-end md:mr-[8vw] items-center bg-transparent transition-[background] duration-1000 p-2 [&:has(.tsl.tsa)]:bg-main'>
           <span className='ts tsf'>
             <span className='tsl text-nowrap'>Watching your</span>
